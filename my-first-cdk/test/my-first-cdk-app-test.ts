b40850008c358d5c0e1b8c22209dd5a32feb2c5c
alihanbays <baysal.alihan@outlook.com>
2024-10-21 15:28:46 -0600

    adding cicd and test

---
+++ my-first-cdk/test/my-first-cdk-app-test.ts
@@ -0,0 +1,13 @@
+import { handler } from "../src";
+
+describe('My First CDK App Tests', () => {
+    it('returns name and test', async() => {
+        const output = await handler({ name: 'Alihan '});
+        expect(output).toStrictEqual('Good Job Alihan')
+    });
+
+    it('returns no name and test', async() => {
+        const output = await handler({ name: ''});
+        expect(output).toStrictEqual('Good Job')
+    });
+});
